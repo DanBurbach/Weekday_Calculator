58b68856c4bab7ea49341425db2d2a3db27258d0
Chris Rudnicky and Daniel Burbach <daniel@>
2019-01-09 16:46:17 -0800

    final commit for the day

--- src/weekday_calculator.js
+++ src/weekday_calculator.js
@@ -17,61 +17,57 @@ class WeekCalc {
     }
   }
 
-    yearFit() {
-      let yearSurgery = this.year.toString().split("").splice(2,2);
-      let decadeCapture = yearSurgery.join("");
-      let result = decadeCapture % 12;
-      return result;
-    }
+  yearFit() {
+    let yearSurgery = this.year.toString().split("").splice(2,2);
+    let decadeCapture = yearSurgery.join("");
+    let result = decadeCapture % 12;
+    return result;
+  }
 
-    yearDifference() {
-      let yearSurgery = this.year.toString().split("").splice(2,2);
-      let decadeCapture = yearSurgery.join("");
-      let result = decadeCapture % 12;
-      let answer = decadeCapture - (result * 12);
-      return answer;
-    }
+  yearDifference() {
+    let yearSurgery = this.year.toString().split("").splice(2,2);
+    let decadeCapture = yearSurgery.join("");
+    let result = decadeCapture % 12;
+    let answer = decadeCapture - (result * 12);
+    return answer;
+  }
 
-    yearComparison() {
+  yearComparison() {
+    let yearSurgery = this.year.toString().split("").splice(2,2);
+    let decadeCapture = yearSurgery.join("");
+    let result = decadeCapture % 12;
+    let answer = parseInt(decadeCapture - (result * 12));
+    let comparison = Math.floor(answer / 4);
+    return comparison;
+  }
+
+  anchorDayFinder() {
+
+    if (1800 <= this.year && this.year <= 1899) {
+      return 5;
+    } else if (1900 <= this.year && this.year <= 1999) {
+      return 3;
+    } else if (2000 <= this.year && this.year <= 2099) {
+      return  2;
+    } else if (2100 <= this.year && this.year <= 2199) {
+      return  0;
+    }
+  }
+
+  doomsdayFinder() {
+    if(1800 > this.year && this.year > 2199) {
+      return "Outside of acceptable date range";
+    } else {
       let yearSurgery = this.year.toString().split("").splice(2,2);
       let decadeCapture = yearSurgery.join("");
       let result = decadeCapture % 12;
       let answer = parseInt(decadeCapture - (result * 12));
       let comparison = Math.floor(answer / 4);
-      return comparison;
-    }
-
-    anchorDayFinder(yyyy) {
-      let returnInt;
-      if (1800 <= yyyy && yyyy <= 1899) {
-        returnInt = 5;
-        return returnInt;
-      } else if (1900 <= yyyy && yyyy <= 1999) {
-        returnInt = 3;
-        return returnInt;
-      } else if (2000 <= yyyy && yyyy <= 2099) {
-        returnInt =  2;
-        return returnInt;
-      } else if (2100 <= yyyy && yyyy <= 2199) {
-        returnInt =  0;
-        return returnInt;
-      }
-    }
-
-    doomsdayFinder() {
-      if(1800 > this.year && this.year > 2199) {
-        let error ="Outside of acceptable date range";
-        return error;
-      } else {
-        let yearSurgery = this.year.toString().split("").splice(2,2);
-        let decadeCapture = yearSurgery.join("");
-        let result = decadeCapture % 12;
-        let answer = parseInt(decadeCapture - (result * 12));
-        let comparison = Math.floor(answer / 4);
-        // let anchorDay = new WeekCalc();
-        let value = this.anchorDayFinder(this.year);
-        let doomsDay = result + answer + comparison + value;
-        return doomsDay % 7;
+      // let anchorDay = new WeekCalc();
+      let value = this.anchorDayFinder();
+      let doomsDay = result + answer + comparison + value;
+      console.log("hello");
+      return doomsDay % 7;
     }
   }
 }
